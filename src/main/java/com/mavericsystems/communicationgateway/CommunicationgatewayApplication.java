553e9a6ae915f76e64aa0ff28860a55758218eea
Vishnu Prasad K <vishnup@maveric-systems.com>
2022-02-11 17:57:20 +0530

    gateway First Commit

---
+++ src/main/java/com/mavericsystems/communicationgateway/CommunicationgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.mavericsystems.communicationgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicationgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunicationgatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mavericsystems/communicationgateway/CommunicationgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mavericsystems.communicationgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
